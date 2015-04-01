62bf4726586b6f556e5d3b9bbbebfe353138c6f5
Tengiz Sharafiev <btolfa@gmail.com>
2015-04-01 09:57:08 +0300

    Empty CMakeLists.txt for tests

---
+++ tests/intelhex/IntelHexTest.cpp
@@ -0,0 +1,26 @@
+/** @brief Тест для проверки работы intelhex библиотеки
+ * 
+ *
+ * @file IntelHexTest.cpp
+ * @date 01 апр. 2015 г.
+ * @author Tengiz Sharafiev <btolfa@gmail.com>
+ */
+
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+namespace {
+
+class IntelHexTest : public ::testing::Test {
+protected:
+	virtual void SetUp() {}
+};
+
+/** @brief @todo { Что должен делать тест }
+*/
+TEST_F(IntelHexTest, Should) {
+	FAIL() << "Not Implemented";
+}
+
+}
